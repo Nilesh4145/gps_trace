4507e63538b4a3917b30a31fcc445d7a45d69025
Nilesh Chaturvedi <Nilesh4145@users.noreply.github.com>
2017-11-14 20:14:15 +0530

    Add files via upload

---
+++ geo_plot.py
@@ -0,0 +1,41 @@
+import numpy
+import gmplot
+import datetime
+
+############# Create time-sorted numpy array of gps trace ##################
+trace_raw = numpy.genfromtxt('gps_trace.csv', delimiter='\t', dtype=str)
+trace = []
+[trace.append(i.split(",")) for i in trace_raw[0:len(trace_raw)-2]]
+trace = numpy.array(trace)
+for i in trace:
+	i[3] = i[3][1:18]
+	i[4] = i[4][1:9]
+sorted_trace = numpy.array(sorted(trace, key=lambda d: d[4]))
+############################################################################
+
+#### Get indices of time stamps corresponding to start and finish time #####
+def left_index(a, arr):
+	index = 0
+	s_time = datetime.datetime.strptime(a, '%H:%M:%S').time()
+	for val in arr:
+		if datetime.datetime.strptime(val, '%H:%M:%S').time() >= s_time:
+			return index
+		index += 1
+
+def right_index(a, arr):
+	index = 0
+	s_time = datetime.datetime.strptime(a, '%H:%M:%S').time()
+	for val in arr:
+		if datetime.datetime.strptime(val, '%H:%M:%S').time() >= s_time:
+			return index-1
+		index += 1
+############################################################################
+
+start_index = left_index("01:00:00", sorted_trace[:, 4])
+finish_index = right_index("02:30:00", sorted_trace[:, 4])
+
+gmap = gmplot.GoogleMapPlotter(sorted_trace[0][0], sorted_trace[0][1], 50)
+gmap.plot(sorted_trace[:, 0][start_index:finish_index].astype(numpy.float), 
+	sorted_trace[:, 1][start_index:finish_index].astype(numpy.float), '#FF6666', edge_width=10)
+
+gmap.draw("map.html")
